8df4482de4501462685783a2f71142be32c2df18
Martin John R. Lopez <Tinjo@Martins-MacBook-Pro.local>
2017-01-17 14:52:34 +0800

    change position of add teams

--- routes/nbateams.js
+++ routes/nbateams.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 			res.render('nbateams', {
 				title: 'NBA Teams',
 				nbateamsData: nbateamsData,
-        moment: moment
+                moment: moment
 			});
 		});
 	});
