da81e01929c0739785e6f68ba4297dfb1a48f168
Martin John R. Lopez <Tinjo@Martins-MacBook-Pro.local>
2017-02-06 02:57:57 +0800

    add restify and method override

--- models/usersdata.js
+++ models/usersdata.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
+var timestamps = require('mongoose-timestamp');
 
 var usersDataSchema = new Schema
 (
@@ -15,5 +16,5 @@ var usersDataSchema = new Schema
 );
 
 usersDataSchema.plugin(passportLocalMongoose);
-
+usersDataSchema.plugin(timestamps);
 usersData = mongoose.model('usersData', usersDataSchema);
